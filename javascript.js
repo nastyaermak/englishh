89e0e00a1f98e86402adb0c487a3b52a2efb1dcc
nastyaermak <149822483+nastyaermak@users.noreply.github.com>
2023-12-08 17:36:06 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+
+   function digitalClock() {    var date = new Date();
+    var hours = date.getHours();    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();       //* додавання ведущіх нулів */
+      if (hours < 10) hours = "0" + hours;      if (minutes < 10) minutes = "0" + minutes;
+      if (seconds < 10) seconds = "0" + seconds;        document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds ;
+        setTimeout("digitalClock()", 1000);   }
